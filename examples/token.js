82e111ce91f00fde1ba50e65f6364360ccd97d72
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2022-11-29 11:47:43 -0500

    brought context up to code standard

--- examples/token.js
+++ examples/token.js
@@ -4,7 +4,7 @@ function mint() {
     if (!minted) {
         return "failed mint"
     }
-    minted = STD.bank.withdrawTokens(CTX.SENDER, "20" + STD.bank.token)
+    minted = STD.bank.withdrawTokens(CTX.sender, "20" + STD.bank.token)
     if (!minted) {
         return "failed withdraw"
     }
